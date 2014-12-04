c017a8a0591ee91bd675337a194c5a84e73facc9
Jason Acquino <jasonacquino@Jasons-MacBook-Pro.local>
2014-12-04 16:27:44 -0500

    added project files to new repo.

---
+++ acquino_jason_project2.js
@@ -0,0 +1,25 @@
+// Jason Acquino
+// December 3, 2014
+// SDI 1412 Project 2
+
+// Bank Robbery
+
+// Variables
+var bankNames = "First National, Second National, Third National";
+var minutesInside = 5;
+var securityGuardWithGun = true;
+var hdSecurityCameras = false;
+var bankNearHighway = true;
+
+//Prompt
+var robber = prompt("Enter your name bank robber");
+    alert("There's a lot of money out there to take " + robber + ". Let's get to work")
+    
+//Procedure
+
+
+//Boolean Function
+
+//Number Function
+
+//String Function
